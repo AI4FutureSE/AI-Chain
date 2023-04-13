d73ea6ce2fccc974347f1ddcc508fbbb03aa9d17
MulongXie <dsh15325@163.com>
2023-04-13 11:21:00 +1000

    fix navbar bug

--- FrontEnd/aichain/public/js/sapper.js
+++ FrontEnd/aichain/public/js/sapper.js
@@ -7,20 +7,20 @@ $(document).ready(()=>{
         let navLink = $('.nav-link')
         // start scroll
         if (cover.offset().top - $(document).scrollTop() <= -100){
-            nav.css({'width': '100%', 'height':'80px', 'position':'fixed', 'background':'white'})
+            nav.css({'width': '100%', 'height':'80px', 'position':'fixed', 'background':'white', 'top':'0'})
             navLink.css({'color':'var(--first)', 'font-weight':'normal'})
             navLink.mouseover(function() {
-                $(this).css({'color':'whitesmoke'})
+                $(this).css({'color':'navajowhite'})
             }).mouseout(function (){
                 $(this).css({'color':'var(--first)'})
             });
         }
         // back to the top
         else{
-            nav.css({'width': '950px', 'height':'65px', 'position':'', 'background':''})
+            nav.css({'width': '950px', 'height':'65px', 'position':'absolute', 'background':'', 'top':'8px'})
             navLink.css({'color':'whitesmoke', 'font-weight':'normal'})
             navLink.mouseover(function() {
-                $(this).css({'color':'whitesmoke'})
+                $(this).css({'color':'navajowhite'})
             }).mouseout(function (){
                 $(this).css({'color':'whitesmoke'})
             });
